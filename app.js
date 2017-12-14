50942cbb9f0c80e349bf5ca7f316e853cf8857c3
ad240 <34466555+ad240@users.noreply.github.com>
2017-12-13 21:33:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -75,43 +75,9 @@ bot.on('message', message => {
         fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
             if(err) console.error(err);
         })
-    }
-
-    // Purge
-    if (msg.startsWith(prefix + 'PURGE')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
-        // We have to wrap this in an async since awaits only work in them.
-        async function purge() {
-            message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
-
-            if (!message.member.roles.find("name", "Admin")) { // This checks to see if they DONT have it, the "!" inverts the true/false
-                message.channel.send('You need the \`Admin\` role to use this command.'); // This tells the user in chat that they need the role.
-                return; // this returns the code, so the rest doesn't run.
-            }
-
-            // We want to check if the argument is a number
-            if (isNaN(args[0])) {
-                // Sends a message to the channel.
-                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>'); //\n means new line.
-                // Cancels out of the script, so the rest doesn't run.
-                return;
-            }
-
-            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
-            //console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
-
-            // Deleting the messages
-            message.channel.bulkDelete(fetched)
-                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
-
-        }
-
-        // We want to make sure we call the function whenever the purge command is run.
-        purge(); // Make sure this is inside the if(msg.startsWith)
-
-    }
 
     // Flagged Words Notification
-    if(!(message.channel.id == '337587604859912193' || message.channel.id == '297364833756643329')){ //ignores messages in #Staff and #Admin channels
+    if(!(message.channel.id == '337587604859912193' || message.channel.id == '297364833756643329' || message.channel.id == '390688064130777088')){ //ignores messages in #Staff and #Admin channels
         for(var i=0; i<flaggedWords.length; i++){
             if(msg.includes(flaggedWords[i])){
                 bot.channels.get('337587604859912193').send(message.author + ' said the flagged word ' + flaggedWords[i] + ' in ' + message.channel + '.');
